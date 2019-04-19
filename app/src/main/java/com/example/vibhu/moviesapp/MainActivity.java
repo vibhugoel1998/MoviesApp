9dbd152e668659c3696b724d8ac2380d65a4f709
vibhugoel1998 <goelvibhu1998@gmail.com>
2019-04-20 03:08:25 +0530

    second commit with intents

--- app/src/main/java/com/example/vibhu/moviesapp/MainActivity.java
+++ app/src/main/java/com/example/vibhu/moviesapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.vibhu.moviesapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void EnterApp(View view){
+        Intent intent=new Intent(this,OpeningActivity.class);
+        startActivity(intent);
+    }
 }
+
